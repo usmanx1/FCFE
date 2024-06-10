dcdbbe721c49662e6ad11c04d5610e92e336ba15
usmanx1 <119616036+usmanx1@users.noreply.github.com>
2024-06-10 10:38:16 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Specify custom pages directory
+  pagesDir: './src/app',
+  // Add other configuration options as needed
+};
 
 export default nextConfig;
